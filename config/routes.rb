87caa18513f30ade6a6ef7680427465279689230
Nicolás Alonso <nicolas@streaver.com>
2022-08-04 10:38:40 -0300

    Generate User with devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root controller: :home, action: :index
+
+  namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'users/auth'
+  end
 end
