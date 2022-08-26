3da05b89b196bd8b9be65949404b4d54f5ab19e4
Nicolás Alonso <nicolas@streaver.com>
2022-08-26 17:48:02 -0300

    Add seeds for Menus

--- spec/factories/dishes.rb
+++ spec/factories/dishes.rb
@@ -2,6 +2,14 @@
 
 FactoryBot.define do
   factory :dish do
-    name { 'MyString' }
+    name { 'Piza' }
+  end
+
+  trait :main do
+    alternative { false }
+  end
+
+  trait :alternative do
+    alternative { true }
   end
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -13,4 +13,32 @@ describe Dish, type: :model do
 
     it { is_expected.to validate_presence_of :name }
   end
+
+  describe 'scopes' do
+    describe '.main' do
+      let!(:main_dish) { create(:dish, :main) }
+      let!(:alternative_dish) { create(:dish, :alternative) }
+
+      it 'includes dishes without alternative flag' do
+        expect(described_class.mains).to include(main_dish)
+      end
+
+      it 'excludes dishes with alternative flag' do
+        expect(described_class.mains).not_to include(alternative_dish)
+      end
+    end
+
+    describe '.alternative' do
+      let!(:main_dish) { create(:dish, :main) }
+      let!(:alternative_dish) { create(:dish, :alternative) }
+
+      it 'includes dishes with alternative flag' do
+        expect(described_class.alternatives).to include(alternative_dish)
+      end
+
+      it 'excludes dishes without alternative flag' do
+        expect(described_class.alternatives).not_to include(main_dish)
+      end
+    end
+  end
 end
